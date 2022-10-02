b9a36c0e1df0f6618cd65110644a89e48fece1f3
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-02 23:12:39 +0300

    toggle menu for mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
+import Home from "./components/header/home/Home";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className="main">
+        <Home />
+      </main>
     </>
   );
 };

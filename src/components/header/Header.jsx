b1d3e56a4a89265ea95412c6cd59885da9c2f067
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-02 17:54:16 +0300

    navbar responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link">
+              <a href="#home" className="nav__link active-link">
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
@@ -44,6 +44,10 @@ const Header = () => {
               </a>
             </li>
           </ul>
+          <i class="uil uil-times nav__close"></i>
+        </div>
+        <div className="nav__toggle">
+          <i class="uil uil-apps"></i>
         </div>
       </nav>
     </header>

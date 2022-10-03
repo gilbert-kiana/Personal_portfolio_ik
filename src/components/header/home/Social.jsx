061f90aa2a36a68c08ddc70034a56756dc4092ea
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-03 11:58:11 +0300

    update

--- src/components/header/home/Social.jsx
+++ src/components/header/home/Social.jsx
@@ -3,9 +3,27 @@ import React from "react";
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="" className="home__social-icon" target="_blank"></a>
-      <a href="" className="home__social-icon" target="_blank"></a>
-      <a href="" className="home__social-icon" target="_blank"></a>
+      <a
+        href="https://www.instagram.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-airplay"></i>
+      </a>
+      <a
+        href="https://dribbble.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-dribbble"></i>
+      </a>
+      <a
+        href="https://github.com/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
     </div>
   );
 };

aea80f505b62d1f9053afbb93beeedfb5493e53e
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-04 07:59:34 +0300

    landing page updated

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+//1:05
+
+const Info = () => {
+  return (
+    <div className="about__info grid">
+      <div className="about__box">
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">2 years</span>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title">Completed</h3>
+        <span className="about__subtitle">5 projects</span>
+      </div>
+
+      <div className="about__box">
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;

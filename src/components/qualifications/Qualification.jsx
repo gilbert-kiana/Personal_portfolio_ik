495d6c34a58bdfdcb73d37ac4f8bcaef22e2de91
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-09 21:05:22 +0300

    update

--- src/components/qualifications/Qualification.jsx
+++ src/components/qualifications/Qualification.jsx
@@ -1,6 +1,13 @@
-import React from "react";
-// 2:00:00
+import React, { useState } from "react";
+import "./qualification.css";
+
 const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <section className-="qualification section">
       <h2 className="section__title">Qualification</h2>
@@ -8,45 +15,67 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className="qualification__button qualification__active button--flex">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
             <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className="qualification__button button--flex">
+          <div
+            className={
+              toggleState === 2
+                ? "qualification__button qualification__active button--flex"
+                : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
             <i className="uil uil-briefcase-alt qualification__icon"></i>
+            {""}
             Experience
           </div>
-          <div className="qualification__sections">
-            <div className="qualification__content qualification__content-active">
-              <div className="qualification_data">
-                <div>
-                  <h3 className="qualification__title">Web Developer</h3>
-                  <span className="qualification__subtitle"></span>Dedan Kimathi
-                  University of Science and Technology
-                  <div className="qualification__calender">
-                    <i className="uil uil-calender-alt"></i> 2022 - Present
-                  </div>
-                </div>
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
+        </div>
+
+        <div className="qualification__sections">
+          <div
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web Developer</h3>
+                <span className="qualification__subtitle">
+                  Dedan Kimathi University of Science and Technology
+                </span>
+                <div className="qualification__calendar">
+                  <i className="uil uil-calender-alt"></i> 2022 - Present
                 </div>
               </div>
-              <div className="qualification_data">
-                <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
 
-                <div>
-                  <span className="qualification__rounder"></span>
-                  <span className="qualification__line"></span>
-                </div>
-                <div>
-                  <h3 className="qualification__title">3D animator</h3>
-                  <span className="qualification__subtitle"></span>Dedan Kimathi
-                  University of Science and Technology
-                  <div className="qualification__calender">
-                    <i className="uil uil-calender-alt"></i> 2022 - Present
-                  </div>
+            <div className="qualification__data">
+              <div></div>
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+              <div>
+                <h3 className="qualification__title">3D Artist</h3>
+                <span className="qualification__subtitle">Self Taught</span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calender-alt"></i> 2022 - Present
                 </div>
               </div>
             </div>

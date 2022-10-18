b958e5a13a086124be6d61b2fd612ff6b6199602
gilbert-kiana <gilbertintabo@gmail.com>
2022-10-18 20:25:12 +0300

    update on styling

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./testimonials.css";
 import { Data } from "./Data";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 const Testimonials = () => {
   return (
@@ -8,18 +12,18 @@ const Testimonials = () => {
       <h2 className="section__title">My clients say</h2>
       <span className="section__subtitle">Testimonial</span>
 
-      <div className="testimonial__container">
+      <Swiper className="testimonial__container">
         {Data.map(({ id, image, title, description }) => {
           return (
-            <div className="testimonial__card" key={id}>
+            <SwiperSlide className="testimonial__card" key={id}>
               <img src={image} alt="" className="testimonial__img" />
 
               <h3 className="testimonial__name">{title}</h3>
               <p className="testimonial__description">{description}</p>
-            </div>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
